0e6a6024311a9f7e31c80463b9f6165997002172
TIKOsup <temirlan.kystaubayev@gmail.com>
2025-01-19 20:16:17 +0500

    feat: add active states

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function setChart() {
   let weekday, height;
-  let chart = document.getElementsByClassName("chart")[0];
   let data = getChartData();
   data.then(json => {
     console.log(json);
     let maxValue = getMaxValue(json);
     setColorToday();
+    setBarValue(json);
     json.forEach(element => {
       weekday = document.getElementById(`${element.day}-bar`);
       height = (element.amount * 100) / maxValue;
@@ -36,6 +36,22 @@ function setColorToday() {
   let today = new Intl.DateTimeFormat("en-US", options).format(date).toLowerCase();
 
   let bar = document.getElementById(`${today}-bar`);
-  let style = getComputedStyle(document.body);
-  bar.style.backgroundColor = style.getPropertyValue("--cyan");
+  bar.classList.add("today");
+}
+
+function setBarValue(data) {
+  let bars = document.getElementsByClassName("spend");
+  for (let bar of bars) {
+    let day = bar.parentElement.id.slice(0, 3);
+    let value = data.find(obj => obj.day === day).amount;
+    bar.innerText = "$" + value;
+  }
+}
+
+function setVisible(element) {
+  element.children[0].classList.add("current");
+}
+
+function setHidden(element) {
+  element.children[0].classList.remove("current")
 }
